d4364d342b74d2115e7e5cd92fe5942fcbd0029b
SarahSaadain <89971132+SarahSaadain@users.noreply.github.com>
2025-07-09 02:37:38 +0200

    Add script to run BUSCO analysis for species in TE_Analysis

---
+++ TE_Analysis/run_busco_per_species.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Create base output directory
+mkdir -p /mnt/data5/sarah/TE_Analysis/BUSCO_Analysis
+
+# Loop over all input FASTA files
+for fasta in /mnt/data5/sarah/aDNA/D*/raw/ref_genome/*.fna; do
+    # Get species identifier: extract species from the full path
+    # Example: Dhis/raw/ref_genome/genome.fna → Dhis/raw/ref_genome
+    species=$(dirname "${fasta#/mnt/data5/sarah/aDNA/}")
+
+    # Remove the "raw/ref_genome" part to get just the species identifier
+    species=${species%/raw/ref_genome}
+
+    # Define output path inside BUSCO_Analysis
+    species_output_dir=/mnt/data5/sarah/TE_Analysis/BUSCO_Analysis/$species
+    output_dir_busco=$species_output_dir/${species}_busco
+    
+    # Create the output directories
+    mkdir -p "$species_output_dir"
+    mkdir -p "$output_dir_busco"
+
+    dataset_path=/mnt/data5/sarah/TE_Analysis/BUSCO_Analysis/busco_downloads/lineages/drosophilidae_odb12
+
+
+    # log path
+    log_path=$species_output_dir/busco_${species}.log
+
+    echo "Running BUSCO on $fasta → $output_dir_busco"
+
+    nohup python -u /home/sarah/miniconda3/bin/busco  --in $fasta --out $output_dir_busco -l $dataset_path -m genome -c 16 > $log_path 2>&1 &
+
+done
